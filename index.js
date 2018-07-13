bcc68dbbefad5e5ba30b44087b5a21c0df7a6ce1
James Tomasino <james@tomasino.org>
2018-07-13 19:06:36 -0400

    added dependencies for pretty output

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 #!/usr/bin/env node
 'use strict'
 
-const program = require('commander')
+const chalk       = require('chalk');
+const clear       = require('clear');
+const figlet      = require('figlet');
+const program     = require('commander')
+
 program
   .version('1.0.0')
   .option('-a, --add','Add a card')
   .option('-c, --complete','Show completed cards')
   .parse(process.argv); // end with parse to parse through the input
 
-console.log('---')
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('flashleit', { horizontalLayout: 'full' })
+  )
+);
+
 if (program.add) {
-  console.log('Add a card')
+  console.log(
+    chalk.green(
+      'Add a card'
+    )
+  )
 }
 if (program.complete) {
-  console.log('Show completed cards')
+  console.log(
+    chalk.green(
+      'Show completed cards'
+    )
+  )
 }
