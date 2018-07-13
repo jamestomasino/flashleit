84d234ecb75827d7fbdea31009622b757d077e6c
James Tomasino <james@tomasino.org>
2018-07-13 19:16:49 -0400

    pretty spinner

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const chalk       = require('chalk');
 const clear       = require('clear');
 const figlet      = require('figlet');
 const program     = require('commander')
+const Configstore = require('configstore');
+var CLI         = require('clui')
+
+const conf        = new Configstore('flashleit');
+var Spinner     = CLI.Spinner;
 
 program
   .version('1.0.0')
@@ -33,3 +38,26 @@ if (program.complete) {
     )
   )
 }
+
+
+var countdown   = new Spinner(
+  chalk.red(
+    'Exiting in 5 seconds...  '
+  ),
+  ['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']
+);
+countdown.start();
+var number = 5;
+setInterval(function () {
+  number--;
+  countdown.message(
+    chalk.red(
+      'Exiting in ' + number + ' seconds...  '
+    )
+  );
+  if (number === 0) {
+    process.stdout.write('\n');
+    process.exit(0);
+  }
+}, 1000);
+
