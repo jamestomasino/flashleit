fc38f35d8bddc48af003be839768d3b62e3cefc3
James Tomasino <james@tomasino.org>
2018-07-13 21:03:48 -0400

    added more visual POP

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var CLI         = require('clui')
 const l           = console.log
 var diskConf    = new Configstore(pkg.name)
 var Spinner     = CLI.Spinner
-var Progress    = CLI.Progress;
+var Progress    = CLI.Progress
 
 /* [x] Setup Program
  * [x] Check settings
@@ -59,37 +59,51 @@ if (diskConf.has('settings')) {
   diskConf.set('settings', settings)
 }
 
-/* Color styles
+/* Visual Styling
 */
 const error   = chalk.bold.red
 const warn    = chalk.magentaBright
 const info    = chalk.blue
 const title   = chalk.black.bgYellow.bold
+function br () {process.stdout.write('\n')}
+
+/* Pause function
+ *  - display progressbar
+ *  - display spinner
+ *  - wait for time, default
+ *  - return
+ */
+function pause () {
+  return new Promise((resolve) => {
+    br()
+    var thisProgressBar = new Progress(45)
+
+    var countdown = new Spinner(
+      warn(thisProgressBar.update(0)),
+      ['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']
+    )
+    countdown.start()
+    var number = 1
+    setInterval(() => {
+      countdown.message(warn(thisProgressBar.update(number, 100)))
+      number+=2
+      if (number >= 100) {
+        resolve()
+      }
+    }, 50)
+  })
+}
 
 /* Shutdown function
- *  - display countdown spinner for shutdown
+ *  - display pause
  *  - save all data
  *  - clear screen
  *  - exit
  */
-function die () {
-  process.stdout.write('\n');
-  var thisProgressBar = new Progress(45);
-
-  var countdown = new Spinner(
-    warn(thisProgressBar.update(0)),
-    ['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']
-  )
-  countdown.start();
-  var number = 1
-  setInterval(() => {
-    countdown.message(warn(thisProgressBar.update(number, 100)))
-    number+=2
-    if (number >= 100) {
-      clear()
-      process.exit(0)
-    }
-  }, 50)
+async function die () {
+  await pause()
+  br()
+  process.exit(0)
 }
 
 program
@@ -98,33 +112,33 @@ program
   .option('-c, --complete','Show completed cards')
   .option('-m, --maxlevels <n>', 'Set maximum proficiency levels', parseInt)
   .option('-d, --debug')
-  .parse(process.argv); // end with parse to parse through the input
+  .parse(process.argv) // end with parse to parse through the input
 
-clear();
-
-// Startup Screen
-l(title(figlet.textSync(' flashleit ', { horizontalLayout: 'full' })));
+// Initialize Screen, display header
+clear()
+l(title(figlet.textSync(' flashleit ', { horizontalLayout: 'full' })))
+br()
 
 // Check for arguments
 if (program.complete) {
   l(info('Show completed cards'))
-  die();
+  die()
 }
 
 if (program.add) {
   l(info('Add a card'))
-  die();
+  die()
 }
 
 if (program.maxlevels > 1) {
   l(info('Set maximum proficiency levels to %s'), program.maxlevels)
   settings.maxlevels = program.maxlevels
   diskConf.set('settings.maxlevels', program.maxlevels)
-  die();
+  die()
 }
 
 if (program.debug) {
   l(info( 'settings: %j'), settings)
 }
 
-die();
+die()
